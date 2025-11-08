d23cdfb5f10a14475e20c4572fa5be71e1a27518
manigandan-d <manigandan220916@gmail.com>
2025-11-08 15:31:49 +0530

    Add text on live camera frame

---
+++ add_text_on_frame.py
@@ -0,0 +1,28 @@
+import cv2
+
+dispW = 640
+dispH = 480
+flip = 2
+
+gst_pipeline = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! \
+          nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! \
+          videoconvert ! video/x-raw, format=BGR ! appsink'
+
+cam = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
+
+while True:
+    ret, frame = cam.read()
+    
+    if not ret:
+        print("Camera not detected")
+        break
+
+    cv2.putText(frame, "Hello Jetson!", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("Text", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
