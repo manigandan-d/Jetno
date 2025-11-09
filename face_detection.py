f154dffdd1e49bb5b360bb096707d4fc1f8700ce
manigandan-d <manigandan220916@gmail.com>
2025-11-09 15:03:48 +0530

    Face detection using OpenCV Haar Cascade

---
+++ face_detection.py
@@ -0,0 +1,43 @@
+import cv2
+
+dispW = 640
+dispH = 480
+flip = 2
+
+gst_pipeline = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! \
+          nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! \
+          videoconvert ! video/x-raw, format=BGR ! appsink'
+
+cam = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
+
+face_cascade = cv2.CascadeClassifier("opencv/data/haarcascades/haarcascade_frontalface_default.xml")
+
+print("Starting...")
+
+while True:
+    ret, frame = cam.read()
+
+    if not ret:
+        print("Camera not detected")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1, 
+        minNeighbors=5,   
+        minSize=(60, 60)  
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # cv2.putText(frame, "Face", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
